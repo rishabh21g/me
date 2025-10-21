0437e658c48f1b98412b6ee4e331c8ec56408de4
rishabh21g <biasedengineer@gmail.com>
2025-10-21 13:44:05 +0530

    text size fix

--- src/components/resume-card.tsx
+++ src/components/resume-card.tsx
@@ -58,7 +58,7 @@ export const ResumeCard = ({
         <div className="flex-grow ml-4 items-center flex-col group">
           <CardHeader>
             <div className="flex items-center justify-between gap-x-2 text-base">
-              <h3 className="inline-flex items-center justify-center font-semibold leading-none text-lg sm:text-sm">
+              <h3 className="inline-flex items-center justify-center font-semibold leading-none md:text-lg text-sm sm:text-sm">
                 {title}
                 {badges && (
                   <span className="inline-flex gap-x-1">
